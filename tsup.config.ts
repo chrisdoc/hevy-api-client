b43173adcec7089502a14cb19eed286f10a51c67
Christoph Kieslich <c.kieslich@gmail.com>
2025-07-19 16:27:47 +0200

    feat(build): configure tsup and add tsconfig

---
+++ tsup.config.ts
@@ -0,0 +1,15 @@
+import type { Options } from 'tsup';
+
+const config: Options = {
+  entry: ['src/index.ts'],
+  cjsInterop: true,
+  clean: true,
+  dts: true,
+  format: ['esm', 'cjs'],
+  minify: false,
+  shims: true,
+  sourcemap: true,
+  splitting: true,
+};
+
+export default config;
